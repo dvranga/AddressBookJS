14f1fb4f9a736fb4cffb967ca8a0a4a7e02872ce
dvranga <devangmranganath@gmail.com>
2020-12-01 10:13:32 +0530

    [ranganath]Added:Ability to ensure valid contacts are added

--- AddressBook.js
+++ AddressBook.js
@@ -1,14 +1,13 @@
+
+const NAME_REGEX = RegExp('^[A-Z]{1}[a-z]{2,}$');
+const ADDRESS_REGEX = RegExp('^[a-zA-z]{3,}$');
+const ZIP_REGEX = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
+const PHONE_NUMBER_REGEX = RegExp('^(0/91)?[6-9][0-9]{9}$'); 
+const EMAIL_REGEX=RegExp('^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$');
+
 class AddressBook{
     // properties
-    firstName;
-    lastName;
-    address;
-    city;
-    state;
-    zip;
-    phoneNumber;
-    email;
-
+    
     constructor(...params){
         this.firstName=params[0];
         this.lastName=params[1];
@@ -20,12 +19,106 @@ class AddressBook{
         this.email=params[7]
     }
 
+    get firstName(){
+        return this._firstName;
+    }
+    set firstName(firstName){
+        if(NAME_REGEX.test(firstName)){
+            return this._firstName=firstName;
+        }
+        else throw( "first name is incoorect");
+    }
+
+
+
+    get lastName(){
+        return this._lastName;
+    }
+    set lastName(lastName){
+        if(NAME_REGEX.test(lastName)){
+            return this._lastName=lastName;
+        }
+        else throw( "last name is incoorect");
+    }
+
+    get address(){
+        return this._address;
+    }
+
+    set address(address){
+        if(ADDRESS_REGEX.test(address)){
+            return this._address=address;
+        }
+        else throw (" address is Incorrect ")
+    }
+
+    get city(){
+        return this._city;
+    }
+
+    set city(city){
+       if(ADDRESS_REGEX.test(city)){
+        return this._city=city;
+       }
+       else throw ("City is Incorrect");
+    }
+
+    get state(){
+        return this._state;
+    }
+
+    set state(state){
+        if(ADDRESS_REGEX.test(state)){
+            return this._state=state;
+        }
+        else throw ("state is Incorrect");
+    }
+
+    get zip(){
+        return this._zip;
+    }
+
+    set zip(zip){
+        if(ZIP_REGEX.test(zip)){
+            return this._zip=zip;
+        }
+        else throw ("Zip is Incorrect");
+    }
+
+    get phoneNumber(){
+        return this._phoneNumber;
+    }
+
+    set phoneNumber(phoneNumber){
+        if(PHONE_NUMBER_REGEX.test(phoneNumber)){
+            return this._phoneNumber=phoneNumber;
+        }
+        else throw ("Phone number is Incorrect");
+    }
+
+    get email(){
+        return this._email;
+    }
+
+    set email(email){
+        if(EMAIL_REGEX.test(email)){
+            return this._email=email;
+        }
+        else throw ("Email is Incorrect");
+    }
 
     toString(){
-        return 'firstName ',this.firstName,' lastName ',this.lastName, ' city ',this.city,' phoneNumber ',this.phoneNumber,' emial ',this.email;
+        return 'firstName ',this.firstName,' lastName ',this.lastName, ' city ',this.city,' phoneNumber ',
+        this.phoneNumber,' emial ',this.email,'address',this.address,'state',this.state,'zip',this.zip;
     }
 
 
 }
-let contact= new AddressBook("ranganath","vatti","gorantla","anatapur","A.p",515231,7483247032,"devangmranganth@gmail.com");
-console.log(contact)
+
+try{
+    let contact= new AddressBook("Ranganath","Vatti","gorantla","anatapur","andrapradesh",515231,7483247032,"devangmranganth@gmail.com");
+    console.log(contact);
+}
+catch(e){
+    console.error(e);
+}
